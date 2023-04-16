5543aa3f67708be3da5e1764bdeefda0886c561b
Yiğitcan Ölmez <yigitcanolmez@outlook.com>
2023-04-16 21:46:01 +0300

    swift giriş

--- FirstProject.playground/Contents.swift
+++ FirstProject.playground/Contents.swift
@@ -5,5 +5,8 @@ var programmerJoke = "Hello, world!"
 var  tryingToUnderstandVariables = 0;
 
 tryingToUnderstandVariables = tryingToUnderstandVariables + 20
+
+tryingToUnderstandVariables = tryingToUnderstandVariables / 2
+
 // ı understood that we can try our codes in there
 
