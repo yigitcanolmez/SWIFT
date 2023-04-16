e6cd16c67a8556cbfb12f5c7180f194e3818a9e6
Yiğitcan Ölmez <yigitcanolmez@outlook.com>
2023-04-16 21:44:05 +0300

    Initial Commit

---
+++ FirstProject.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var programmerJoke = "Hello, world!"
+
+var  tryingToUnderstandVariables = 0;
+
+tryingToUnderstandVariables = tryingToUnderstandVariables + 20
+// ı understood that we can try our codes in there
+
