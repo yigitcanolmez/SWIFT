ae5497b3a19c2fad37676bef3080d1c67fa98e86
Yiğitcan Ölmez <yigitcanolmez@outlook.com>
2023-04-22 19:09:39 +0300

    Widgets - Button

---
+++ IOS_GIRIS/IOS_GIRIS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_GIRIS
+//
+//  Created by Yiğitcan Ölmez on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
